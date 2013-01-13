9157bd2c56df749fed3b60cfa6873e3b7b3383b1
sumito3478 <sumito3478@gmail.com>
2013-01-14 05:37:02 +0900

    Bump version number to 0.0.2-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object AprikotCombinatorBuild extends Build {
   ).settings(StandardProject.newSettings :_*
   ).settings(
     Seq(
-      version := "0.0.1"
+      version := "0.0.2-SNAPSHOT"
     ): _*
   )
 
